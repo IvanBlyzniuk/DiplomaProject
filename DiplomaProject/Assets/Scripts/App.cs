c7fac252c3db80f01d86240b75b3c5ef6dfb7a74
IvanBlyzniuk <84847629+IvanBlyzniuk@users.noreply.github.com>
2024-01-04 14:09:40 +0200

    +basic project structure

---
+++ DiplomaProject/Assets/Scripts/App.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using World;
+
+public class App : MonoBehaviour
+{
+
+    [SerializeField] private ObjectsContainer objectsContainer;
+
+    void Start()
+    {
+        
+    }
+
+}
